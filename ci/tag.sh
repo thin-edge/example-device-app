215191421f40b33122b5b542dbb86750ef05032c
reubenmiller <reuben.d.miller@gmail.com>
2025-03-11 17:24:01 +0100

    ci: add build workflow

---
+++ ci/tag.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+if [ -n "$CI" ]; then
+    set -x
+fi
+bump="$1"
+
+CURRENT_VERSION=$(git describe --tags --abbrev=0)
+
+case "$bump" in
+    patch)
+        NEXT_VERSION=$(echo "$CURRENT_VERSION" | awk -F. '{$NF+=1; OFS="."; print $1"."$2"."($3 + 1)}')
+        ;;
+    minor)
+        NEXT_VERSION=$(echo "$CURRENT_VERSION" | awk -F. '{$NF+=1; OFS="."; print $1"."($2 + 1)"."$3}')
+        ;;
+    major)
+        NEXT_VERSION=$(echo "$CURRENT_VERSION" | awk -F. '{$NF+=1; OFS="."; print ($1 + 1)"."$2"."$3}')
+        ;;
+esac
+
+git config --global user.email "info@thin-edge.io"
+git config --global user.name "Versioneer"
+echo "Creating tag: $NEXT_VERSION"
+git tag -a "$NEXT_VERSION" -m "Release $NEXT_VERSION"
+git push origin "$NEXT_VERSION"
